6e4d847e964f899bfbdb2eeaa47ddde8d049b285
bruno brandao alves vieira <bruno_11brandao@hotmail.com>
2017-05-22 11:43:52 -0300

    primeiro commit

--- TiGrafosLinhaAerea/Form1.cs
+++ TiGrafosLinhaAerea/Form1.cs
@@ -12,9 +12,15 @@ namespace TiGrafosLinhaAerea
 {
     public partial class Form1 : Form
     {
+        ManipulaArquivo arquivo;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             arquivo = new ManipulaArquivo();
         }
     }
 }
---
+++ TiGrafosLinhaAerea/ManipulaArquivo.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+
+namespace TiGrafosLinhaAerea
+{
+    class ManipulaArquivo
+    {
+        public int[] a = new int[1000000];
+        int posicao = 0;
+
+
+        public ManipulaArquivo()
+        {
+            importArquivo();
+        }
+        void importArquivo()
+        {
+            StreamReader leitor = new StreamReader("C:/Users/bruno/Google Drive/PUC/12017/GRAFOS/TiGrafosLinhaAerea/TiGrafosLinhaAerea/Ordena.txt");
+            string atual = leitor.ReadLine();
+
+            while(atual != null)
+            {
+                a[posicao] = int.Parse(atual);
+                posicao++;
+                atual = leitor.ReadLine();
+            }
+            leitor.Close();
+        }
+    }
+}
